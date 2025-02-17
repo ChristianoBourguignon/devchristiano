a51b3bee2256490961b2fe5d2ab9f40d66c99ebc
ChristianoBourguignon <christianombl@gmail.com>
2025-02-17 03:38:08 -0300

    Starter project

---
+++ templates/nav.php
@@ -0,0 +1,26 @@
+<nav class="navbar bg-dark border-bottom border-body navbar-expand-lg fixed-top" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="../img/dev-logo.png" alt="Logo" width="50" height="44" class="d-inline-block align-text-top">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="home" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="Sobre mim" href="#">Sobre mim</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="Meus projetos" href="#">Projetos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="Contato" href="../pages/contato.php">Contato</a>
+                </li>
+            </ul>
+        <span class="navbar-text">
+        Tenha sua solução na web!
+        </span>
+        </div>
+    </div>
+</nav>
