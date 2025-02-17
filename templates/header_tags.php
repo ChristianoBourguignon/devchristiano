fc156861b21b33dbd728b270d36252a7847d4b3a
ChristianoBourguignon <christianombl@gmail.com>
2025-02-17 17:06:24 -0300

    Deploy test deploy.yml

--- templates/header_tags.php
+++ templates/header_tags.php
@@ -32,7 +32,7 @@ include  __DIR__ . '/config.php'
         }
     ?>    
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="/img/Dev.ico">
+    <link rel="icon" type="image/x-icon" href="<?= BASE_URL ?>img/Dev.ico">
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
