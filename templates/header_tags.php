8cb59764d08b8f86984b47b7a6cd111d73e5b279
ChristianoBourguignon <christianombl@gmail.com>
2025-02-17 16:40:19 -0300

    adjust ways relative for ico and logo

--- templates/header_tags.php
+++ templates/header_tags.php
@@ -6,7 +6,7 @@ include  __DIR__ . '/config.php'
 <html lang="pt-BR">
 <head>
     <?php 
-    // $_SERVER['REQUEST_URI'] eu obtenho o caminho da url a partir da BASE_URL
+    // $_SERVER['REQUEST_URI'] eu obtenho o caminho da url
 
     switch($_SERVER['REQUEST_URI']){
         case BASE_URL . 'pages/sobre.php':
@@ -32,11 +32,11 @@ include  __DIR__ . '/config.php'
         }
     ?>    
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="/dev-christiano/img/Dev.ico">
+    <link rel="icon" type="image/x-icon" href="<?= BASE_URL .'/img/Dev.ico '?>">
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="/dev-christiano/styles/base.css" rel="stylesheet">
+    <link href="<?= BASE_URL .'/styles/base.css' ?>" rel="stylesheet">
 </head>
 <body>
 <?php include  __DIR__ . '/nav.php'; ?> 
