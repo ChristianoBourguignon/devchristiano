2005cf35f8d0bc353cc37adc524f268c2353ccd7
ChristianoBourguignon <christianombl@gmail.com>
2025-02-18 22:42:45 -0300

    Fixed menu hamburguer

--- templates/nav.php
+++ templates/nav.php
@@ -3,6 +3,9 @@
         <a class="navbar-brand" href="<?= BASE_URL ?>">
             <img src="<?= BASE_URL ?>img/dev-logo.png" alt="Logo" width="50" height="44" class="d-inline-block align-text-top">
         </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
