bb2b32df2d83eef99baccca89892764b391b82d2
CHRISTIANO MONTEIRO BOURGUIG <alun23204708360066@h5-10.faculdade.edu>
2025-02-19 20:42:37 -0300

    Ajuste de responsividade

--- pages/home.php
+++ pages/home.php
@@ -4,11 +4,21 @@
 include __DIR__ . '/../templates/header_tags.php';
 ?>
 
-<div class="container-fluid">
-    <h1>Home</h1>
-    <p>Aqui esta minha página principal</p>
+<div class="container text-center py-2">
+    <div class="row">
+        <div class="col">
+            <img src="<?= BASE_URL ?>img/person.jpeg" class="rounded img-fluid" alt="Pessoa">
+        </div>
+        <div class="col text-start">
+            olá
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"> </div>
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> </div>
+            <div class="progress-bar" style="width: 25%"></div>
+        </div>
+    </div>
+</div>
 </div>
 
 <?php
-include  __DIR__ .'/../templates/footer.php';
+include  __DIR__ . '/../templates/footer.php';
 ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,22 @@
-<footer class="bg-dark text-white text-center py-3">
+<footer class="bg-dark text-white text-center py-2 w-100">
     <div class="container">
         <div class="d-flex justify-content-center mb-3">
-            <a href="https://github.com/ChristianoBourguignon/" class="text-white mx-3"><i class="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/christiano-bourguignon-228a37181/" class="text-white mx-3"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/ChristianoBourguignon/" class="text-white mx-3">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/christiano-bourguignon-228a37181/" class="text-white mx-3">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
         </div>
         <div class="mb-3">
-            <img src="/img/dev-logo.png" alt="Logo" width="30px" height="30px" class="img-fluid" style="max-height: 50px;">
+            <img src="<?php BASE_URL ?>img/dev-logo.png" alt="Logo" class="img-fluid" style="max-height: 50px; width: auto;">
         </div>
         <p class="mb-0">&copy; <?php print date("Y"); ?> Todos os direitos reservados.</p>
     </div>
 </footer>
 
-<?php 
-include __DIR__ .'/../templates/footer_scripts.php';
+<?php
+include __DIR__ . '/../templates/footer_scripts.php';
 ?>
 </body>
 </html>
