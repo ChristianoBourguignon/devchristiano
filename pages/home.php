942f7f35273b4804853c263437e2fe956bb166af
ChristianoBourguignon <christianombl@gmail.com>
2025-02-25 08:45:33 -0300

    Deploy

--- pages/home.php
+++ pages/home.php
@@ -9,25 +9,32 @@ include __DIR__ . '/../templates/header_tags.php';
         <div class="col">
             <img src="img/person.jpeg" class="rounded img-fluid" alt="Pessoa">
         </div>
-        <div class="col text-start">
-            <div class="animation-typing text-center p-2 w-100">Olá, meu nome é Christiano</div>
-            <section id="conhecimentoWordpress">
+        <div class="col text-center">
+            <div class="animation-typing p-2 w-100">Olá, meu nome é Christiano</div>
+            <section class="mb-5">
                 <div class="d-flex flex-row mb-3 align-items-center">
                     Meu conhecimento em <i class="bi bi-wordpress h2 p-1"></i>
                 </div> 
                 <div class="progress">
-                    <div class="progress-bar bg-success" role="progressbar" aria-label="Segment two" style="width: 75%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"><i class="bi bi-wordpress"></i></div>
+                    <div class="progress-bar bg-success" role="progressbar" aria-label="Segment two" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
                 </div>
             </section>
-            <div class="offcanvas offcanvas-start show text-bg-dark" tabindex="-1" id="offcanvasDark" aria-labelledby="offcanvasDarkLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasDarkLabel">Offcanvas</h5>
-    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvasDark" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <p>Place offcanvas content here.</p>
-  </div>
-</div>
+            <section class="mb-5">
+                <div class="d-flex flex-row mb-3 align-items-center">
+                    Meu conhecimento em <i class="bi bi-mortarboard-fill h2 p-1"></i> (Criação de Cursos)</i>
+                </div> 
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" aria-label="Segment two" style="width: 81%" aria-valuenow="81" aria-valuemin="0" aria-valuemax="100">81%</div>
+                </div>
+            </section>
+            <section class="mb-5">
+                <div class="d-flex flex-row mb-3 align-items-center">
+                    Meu conhecimento em <i class="bi bi-filetype-php h2 p-1"></i></i>
+                </div> 
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" aria-label="Segment two" style="width: 81%" aria-valuenow="81" aria-valuemin="0" aria-valuemax="100">81%</div>
+                </div>
+            </section>
         </div>
     </div>
 </div>
